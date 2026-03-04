1ae665d37fef1cac4059dbd5107bda0f5d754ef9
suvarnajichkar <suvarnajichkar204@gmail.com>
2026-03-04 13:06:50 +0000

    create doctype leave types,leave allocation,leave application

--- saral_hr/saral_hr/doctype/leave_allocation/leave_allocation.py
+++ saral_hr/saral_hr/doctype/leave_allocation/leave_allocation.py
@@ -1,9 +1,9 @@
-# Copyright (c) 2026, sj and contributors
-# For license information, please see license.txt
-
-# import frappe
+import frappe
 from frappe.model.document import Document
 
-
 class LeaveAllocation(Document):
-	pass
+    def validate(self):
+        if not self.used_leaves:
+            self.used_leaves = 0
+
+        self.remaining_leaves = self.total_allocated - self.used_leaves
