1c2f9d40556076e3c9d48d8ee4d743d37a2ad477
suvarnajichkar <suvarnajichkar204@gmail.com>
2026-03-13 12:12:44 +0000

    fix: filter company dropdown by user permission in attendance dashboard

--- saral_hr/saral_hr/doctype/salary_component/salary_component.js
+++ saral_hr/saral_hr/doctype/salary_component/salary_component.js
@@ -28,19 +28,12 @@ frappe.ui.form.on('Salary Component', {
     // Only one payment-day mode may be active at a time
 
     depends_on_payment_days(frm) {
-<<<<<<< HEAD
-        if (frm.doc.depends_on_payment_days) {
-            if (frm.doc.is_daily_rate)                    frm.set_value('is_daily_rate', 0);
-            if (frm.doc.depends_on_physical_working_days) frm.set_value('depends_on_physical_working_days', 0);
-            frappe.show_alert({ message: __('Other payment dFesicay flags have been unchecked. A component can only have one calculation mode.'), indicator: 'orange' });
-=======
         if (frm.doc.depends_on_payment_days && frm.doc.depends_on_physical_working_days) {
             frm.set_value('depends_on_physical_working_days', 0);
             frappe.show_alert({
                 message: __('Depends on Physical Working Days has been unchecked. A component can only have one calculation mode.'),
                 indicator: 'orange'
             });
->>>>>>> origin/daisy
         }
     },
 
